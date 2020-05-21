ac9b8b2bca08c6c76d2031f0f9903b8d527a61fc
sou <soueleven@gmail.com>
2020-05-21 22:38:42 +0900

    Initial Commit

---
+++ SnackGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SnackGame
+//
+//  Created by Song on R 2/05/20.
+//  Copyright © Reiwa 2 Sou. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
